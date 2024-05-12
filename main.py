447ec876a080f57acd756342d23d46806fc3c28c
lukexiao <814008000@163.com>
2024-05-12 16:06:58 +0800

    1

--- main.py
+++ main.py
@@ -2,5 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+with open('./hello.js', 'r') as file:
+    hello_content = file.read()
+
+
+
 site_title="klpalace"
-st.title(site_title)
+st.header(site_title,divider='rainbow')
+
+st.write(1234)
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
+
+st.code(hello_content,language='javascript')
